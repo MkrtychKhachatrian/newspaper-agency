e9d7f885c012e8f8f9f717746fabcc247ddcdf51
Mkrtych Khachatrian <mukuchbrat@gmail.com>
2023-05-03 19:39:46 +0300

    Feat: add models Topic, Redactor, Newspaper

--- agency/models.py
+++ agency/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Topic(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Redactor(AbstractUser):
+    years_of_experience = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name})"
+
+
+class Newspaper(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    published_date = models.DateTimeField(auto_now_add=True)
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE, related_name="newspapers")
+    publishers = models.ManyToManyField(Redactor, related_name="newspapers")
+
+    def __str__(self):
+        return f"{self.title} ({self.topic})"
