b2d0eea0db716e6327f9956ab6bb2cb3deb82d5c
Mkrtych Khachatrian <mukuchbrat@gmail.com>
2023-05-04 20:00:06 +0300

    Feat: add index view & templates

--- agency/views.py
+++ agency/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+from agency.models import Redactor, Newspaper, Topic
+
+
+def index(request):
+    num_redactors = Redactor.objects.count()
+    num_newspapers = Newspaper.objects.count()
+    num_topics = Topic.objects.count()
+
+    context = {
+       "num_drivers": num_redactors,
+       "num_cars": num_newspapers,
+       "num_manufacturers": num_topics,
+    }
+
+    return render(request, "agency/index.html", context=context)
