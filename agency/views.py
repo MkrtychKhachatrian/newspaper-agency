e0b771ac4e5a80c45cc088386a7b00724dab9af3
Mkrtych Khachatrian <mukuchbrat@gmail.com>
2023-05-08 14:27:26 +0300

    Add registration & crispy bootstrap

--- agency/views.py
+++ agency/views.py
@@ -1,17 +1,19 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 from agency.models import Redactor, Newspaper, Topic
 
 
+@login_required
 def index(request):
     num_redactors = Redactor.objects.count()
     num_newspapers = Newspaper.objects.count()
     num_topics = Topic.objects.count()
 
     context = {
-       "num_drivers": num_redactors,
-       "num_cars": num_newspapers,
-       "num_manufacturers": num_topics,
+       "num_redactors": num_redactors,
+       "num_newspapers": num_newspapers,
+       "num_topics": num_topics,
     }
 
     return render(request, "agency/index.html", context=context)
--- newspaper_agency/settings.py
+++ newspaper_agency/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "debug_toolbar",
+    "crispy_forms",
+    "crispy_bootstrap5",
     "agency",
 ]
 
@@ -69,6 +71,10 @@ TEMPLATES = [
         },
     },
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 WSGI_APPLICATION = "newspaper_agency.wsgi.application"
 
