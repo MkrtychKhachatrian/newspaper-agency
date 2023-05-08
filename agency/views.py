ab2c782e674a1ea6cca6202d834376a8997b498f
Mkrtych Khachatrian <mukuchbrat@gmail.com>
2023-05-08 15:26:49 +0300

    Add TopicListView & topic_list.html template

--- agency/views.py
+++ agency/views.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from django.views import generic
 
 from agency.models import Redactor, Newspaper, Topic
 
@@ -17,3 +19,31 @@ def index(request):
     }
 
     return render(request, "agency/index.html", context=context)
+
+
+class TopicListView(LoginRequiredMixin, generic.ListView):
+    model = Topic
+    context_object_name = "topic_list"
+    template_name = "agency/topic_list.html"
+    paginate_by = 5
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(TopicListView, self).get_context_data(**kwargs)
+
+        name = self.request.GET.get("name", "")
+
+        context["search_form"] = TopicSearchForm(initial={"name": name})
+
+        return context
+
+    def get_queryset(self):
+        queryset = Topic.objects.all()
+
+        form = TopicSearchForm(self.request.GET)
+
+        if form.is_valid():
+            return queryset.filter(
+                name__icontains=form.cleaned_data["name"]
+            )
+
+        return queryset
