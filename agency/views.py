02e24c9e123b8f0674a358573a2db7e0fec7a0ab
Mkrtych Khachatrian <mukuchbrat@gmail.com>
2023-05-08 15:32:35 +0300

    Feat: add TopicSearchForm

--- agency/views.py
+++ agency/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.views import generic
 
+from agency.forms import TopicSearchForm
 from agency.models import Redactor, Newspaper, Topic
 
 
