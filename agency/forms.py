1b7cfc1a6ca05ffe3b1545e0589cdb95a0ceff51
Mkrtych Khachatrian <mukuchbrat@gmail.com>
2023-05-09 18:48:31 +0300

    Feat: add RedactorExperienceUpdateForm & validator

--- agency/forms.py
+++ agency/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 
+from agency.validators import validate_years_of_experience
+
 
 class TopicSearchForm(forms.Form):
     name = forms.CharField(
@@ -29,3 +31,16 @@ class RedactorCreationForm(UserCreationForm):
             "first_name",
             "last_name",
         )
+
+    def clean_years_of_experience(self):
+        return validate_years_of_experience(self.cleaned_data["years_of_experience"])
+
+
+class RedactorExperienceUpdateForm(forms.ModelForm):
+    class Meta:
+        model = get_user_model()
+        fields = ["years_of_experience"]
+
+    def clean_years_of_experience(self):
+        return validate_years_of_experience(self.cleaned_data["years_of_experience"])
+
